b05d18be63a0ffe2fa3413ff789b5ef9806abeef
jonhsel <jonhselmo.engcomp@gmail.com>
2024-04-23 10:27:56 -0300

    criado a rotina de carregar os transformadores

--- modelos/md_log_deploy.py
+++ modelos/md_log_deploy.py
@@ -9,4 +9,9 @@ import joblib
 # App
 app = Flask(__name__)
 
+# Carrega o modelo e transformadores do disco
+mdl_logistica = joblib.load('modelos/modelo_logistica.plk')
+le_tipo_embalagem = joblib.load('modelos/transformador_tipo_embalagem.plk')
+le_tipo_produto = joblib.load('modelos/transformador_tipo_produto.plk')
+
 
