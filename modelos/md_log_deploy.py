9029740467ea11db6f56c5771c67bc01bcf3f6c7
jonhsel <jonhselmo.engcomp@gmail.com>
2024-04-23 10:19:33 -0300

    bibliotecas importadas e definido app

--- modelos/md_log_deploy.py
+++ modelos/md_log_deploy.py
@@ -1,2 +1,12 @@
-#Projeto1  - Construção e Deploy de Modelo de Machine Learning para a área de logística
+# Projeto1  - Construção e Deploy de Modelo de Machine Learning para a área de logística
+
+# implementação do software e Deploy do Modelo
+
+# Bibliotecas
+from flask import Flask, render_template, request, jsonify
+import joblib
+
+# App
+app = Flask(__name__)
+
 
